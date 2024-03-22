81c90a03c87afa9f3fcd162074e9928de4ebf2ff
Artyom Keydunov <artyom@cube.dev>
2024-03-22 16:22:55 +0000

    Commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -3,11 +3,6 @@
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
     This will override configurations stated in dbt_project.yml
-
-    Try changing "table" to "view" below
-<<<<<<< HEAD
-    Change FROM REMOTE BUT LOCAL NOW
-=======
-    Change FROM REMOTE AGAIN AND AGAIN
->>>>>>> 026be3a642eff5a322a6728ecd0fac6a23b03de3
+ 
+    Cleanup
 */
